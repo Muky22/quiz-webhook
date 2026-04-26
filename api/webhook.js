5e6ed77e95a0289445166f2d4711826c1988153d
Muky22 <47782883+Muky22@users.noreply.github.com>
2026-04-26 13:32:08 +0200

    Add files via upload

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,22 @@
-﻿module.exports = (req, res) => {
-  res.status(200).json({ ok: true })
+﻿module.exports = async (req, res) => {
+  try {
+    const event = req.body
+
+    console.log("EVENT:", event?.type)
+
+    if (event?.type === "checkout.session.completed") {
+      const session = event.data.object
+
+      const email = session?.customer_email
+
+      console.log("PAID USER:", email)
+
+      // sem potom dáme Firestore save
+    }
+
+    res.status(200).json({ received: true })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "fail" })
+  }
 }
