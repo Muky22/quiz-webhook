5c42364187b87fe6c83d872eb2a798184c53282a
Muky22 <47782883+Muky22@users.noreply.github.com>
2026-04-26 16:59:40 +0200

    Add files via upload

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,14 @@
 ﻿const admin = require("firebase-admin")
 
 if (!admin.apps.length) {
-  admin.initializeApp()
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      ...serviceAccount,
+      private_key: serviceAccount.private_key.replace(/\\n/g, "\n"),
+    }),
+  })
 }
 
 const db = admin.firestore()
