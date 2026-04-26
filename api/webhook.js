6a8e993cfe191ed0701b49bb1db02ee93ed47d99
Muky22 <47782883+Muky22@users.noreply.github.com>
2026-04-26 13:27:27 +0200

    Add files via upload

--- api/webhook.js
+++ api/webhook.js
@@ -1,16 +1,16 @@
-﻿export default async function handler(req, res) {
+﻿export default function handler(req, res) {
   try {
     const event = req.body
 
     console.log("Webhook received:", event?.type)
 
-    // TEST: keď Stripe pošle successful payment
+    // Stripe payment success event
     if (event?.type === "checkout.session.completed") {
       const session = event.data.object
 
-      const email = session.customer_email
+      const email = session?.customer_email
 
-      console.log("PAID USER:", email)
+      console.log("💰 PAID USER:", email)
     }
 
     res.status(200).json({ received: true })
